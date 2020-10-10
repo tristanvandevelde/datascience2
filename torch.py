8690d9c033f58f5d942dd41dff5274105f331471
Tristan Vandevelde <tristan@tristanvandevelde.io>
2020-10-10 18:04:12 +0200

    rebase

---
+++ torch.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+from torchvision import transforms, datasets
+
+train = datasets.MNIST("", train=True, download=True, transform = transforms.Compose([transforms.ToTensor()]))
+
+test = datasets.MNST("", train=False, download=True, transform = transforms.Compose([transforms.ToTensor()]))
+
+trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
+# 10 items at a time through the model
+# best: 8-64
+
+
+
+
